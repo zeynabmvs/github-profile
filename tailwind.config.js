55074cbb9e336abc847dcd470ca20a94ab03af93
Zeynab Movasaqpoor <zeynab.mvs@gmail.com>
2024-03-07 14:58:06 +0330

    bug fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,16 @@ export default {
     fontFamily: {
       bevietnam: ["BeVietnamPro"],
     },
-    darkgray: "#111729",
-    darkblue: "#1D1B48",
-    blue: "#3662E3",
-    gray: "#20293A",
-    slate: {
-      100: "#CDD5E0",
-      200: "#4A5567",
-      300: "#364153",
+    colors: {
+      darkgray: "#111729",
+      darkblue: "#1D1B48",
+      blue: "#3662E3",
+      gray: "#20293A",
+      slate: {
+        100: "#CDD5E0",
+        200: "#4A5567",
+        300: "#364153",
+      },
     },
   },
   plugins: [],
